02f813b8a7891227bebafbcea48a726376a024a0
SALEH FAISAL AHMED AL-SHAMI <220208864@ostimteknik.edu.tr>
2025-06-26 19:39:34 +0300

    Update University.js

--- frontend/src/pages/University.js
+++ frontend/src/pages/University.js
@@ -14,7 +14,7 @@ const University = () => {
   useEffect(() => {
     const fetchUniversity = async () => {
       try {
-        const response = await fetch(`http://stdbackend-production.up.railway.app/api/universities/${id}`);
+        const response = await fetch(`https://stdbackend-production.up.railway.app/api/universities/${id}`);
         const data = await response.json();
         setUniversity(data);
       } catch (error) {
