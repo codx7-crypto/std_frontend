d2df353d150db81055b97d9c21311ef35af7f821
SALEH FAISAL AHMED AL-SHAMI <220208864@ostimteknik.edu.tr>
2025-06-26 15:52:57 +0300

    Update BlogPage.js

--- frontend/src/pages/BlogPage.js
+++ frontend/src/pages/BlogPage.js
@@ -9,7 +9,7 @@ const BlogPage = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/blogs')
+    fetch('http://stdbackend-production.up.railway.app/api/blogs')
       .then(res => res.json())
       .then(data => setBlogs(data))
       .catch(err => console.error('Error loading blogs:', err));
